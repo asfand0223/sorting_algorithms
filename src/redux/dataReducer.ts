5c1e7ba62c247d4eecb7aa819bd8643d9281dcc8
Asfand Ali <asfand0223@gmail.com>
2024-12-22 18:07:58 +0000

    Merge sort, quick sort, insertion sort fully implemented

--- src/redux/dataReducer.ts
+++ src/redux/dataReducer.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface IInitialState {
+  unsortedData: number[][];
   data: number[];
   sorted: boolean;
   sortSteps: number[][];
@@ -9,6 +10,7 @@ interface IInitialState {
 }
 
 const initialState: IInitialState = {
+  unsortedData: [],
   data: [],
   sorted: false,
   sortSteps: [],
@@ -79,6 +81,45 @@ const merge = (
   sortSteps.push([...data]);
 };
 
+const quickSort = (
+  data: number[],
+  s: number,
+  e: number,
+  sortSteps: number[][],
+) => {
+  if (e - s + 1 <= 1) return;
+  let pivot = data[e];
+  let k = s;
+
+  for (let i = s; i < e; i++) {
+    if (data[i] < pivot) {
+      let temp = data[k];
+      data[k++] = data[i];
+      data[i] = temp;
+    }
+  }
+
+  data[e] = data[k];
+  data[k] = pivot;
+
+  sortSteps.push([...data]);
+  quickSort(data, s, k - 1, sortSteps);
+  quickSort(data, k + 1, e, sortSteps);
+};
+
+const insertionSort = (data: number[], sortSteps: number[][]) => {
+  for (let i = 0; i < data.length; i++) {
+    let j = i - 1;
+    let elemToSort = data[i];
+    while (j >= 0 && data[j] > elemToSort) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j + 1] = elemToSort;
+    sortSteps.push([...data]);
+  }
+};
+
 export const dataSlice = createSlice({
   name: "data",
   initialState,
@@ -98,13 +139,13 @@ export const dataSlice = createSlice({
     quickSortData: (state: IInitialState) => {
       if (state.sorted) return;
       state.sortSteps = [];
-      mergeSort(state.data, 0, state.data.length - 1, state.sortSteps);
+      quickSort(state.data, 0, state.data.length - 1, state.sortSteps);
       state.sorted = true;
     },
     insertionSortData: (state: IInitialState) => {
       if (state.sorted) return;
       state.sortSteps = [];
-      mergeSort(state.data, 0, state.data.length - 1, state.sortSteps);
+      insertionSort(state.data, state.sortSteps);
       state.sorted = true;
     },
     setCurrentStep: (
